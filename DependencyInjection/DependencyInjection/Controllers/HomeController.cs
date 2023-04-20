c1aa564d2f6792bd595dc3935ee5105396b0022c
Jarvis1952 <neelparghi192@gmail.com>
2023-04-20 22:48:19 +0530

    Dependency Inversion Principle

---
+++ DependencyInjection/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CitiesService _citiesService;
+        
+        public HomeController()
+        {
+            _citiesService = new();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService.GetCities();
+            return View(cities);
+        }
+    }
+}
---
+++ DependencyInjection/Services/CitiesService.cs
@@ -0,0 +1,19 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService()
+        {
+            _cities = new List<string>() {
+                "London", "Paris", "NYC"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
